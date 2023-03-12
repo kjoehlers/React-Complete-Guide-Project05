2e9da6c2bab8255923390e376b3923bec4db197e
Ken Oehlers <kjojr56@gmail.com>
2023-03-12 12:48:03 -0400

    Commit before Lesson 125

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,8 @@
-import React, { useState, useEffect, useReducer } from "react";
+import React, { useState, useEffect, useReducer, useContext } from "react";
 
 import Card from "../UI/Card/Card";
+import AuthContext from "../../store/auth-context";
+
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
 
@@ -40,6 +42,8 @@ const Login = (props) => {
     value: "",
     isValid: null,
   });
+
+  const authCtx = useContext(AuthContext);
 
   useEffect(() => {
     // console.log("EFFECT RUNNING");
@@ -86,7 +90,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    authCtx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
